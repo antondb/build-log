95181d82862183ad585a38305e7a92596724cb18
anton <deblasio36@gmail.com>
2019-09-25 22:30:18 +0100

    Added version 0.0.1

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Build Log 0.0.1`,
+    description: `Projects and Lists.`,
     author: `@gatsbyjs`,
   },
   plugins: [
